3a332302a8fbfd4221f8d4c37b955bd2ad6cfd54
GNikroo <gina.nikroo@gmail.com>
2023-05-09 08:17:20 +0000

    Debug: fix broken default user link

--- profiles/models.py
+++ profiles/models.py
@@ -9,7 +9,7 @@ class Profile(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
-    image = models.ImageField(upload_to='images/', default='../default_user_kl0k9t')  # noqa
+    image = models.ImageField(upload_to='images/', default='../default_user_fsxard')  # noqa
 
     class Meta:
         ordering = ['-created_at']
