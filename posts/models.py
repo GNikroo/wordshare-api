60c1a7cb525ae004450608aca7ce9358fcc8394a
GNikroo <gina.nikroo@gmail.com>
2023-05-15 10:06:31 +0000

    Put back default post image in Post models

--- posts/models.py
+++ posts/models.py
@@ -9,7 +9,7 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', blank=True
+        upload_to='images/', default='../default_post_mu9jl6.jpg',  blank=True
     )
 
     class Meta:
