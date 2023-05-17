e22a4aaa28c4e87708944279ae17bb54a475345c
GNikroo <gina.nikroo@gmail.com>
2023-05-17 08:04:19 +0000

    Debug: Image upload

--- posts/models.py
+++ posts/models.py
@@ -8,6 +8,7 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
+    image = models.ImageField(upload_to='images/', blank=True)
 
     class Meta:
         ordering = ['-created_at']
