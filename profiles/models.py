074a11ab33fa14df40e7df63f341e895893a2e49
GNikroo <gina.nikroo@gmail.com>
2023-05-19 10:09:53 +0000

    Update profile url

--- profiles/models.py
+++ profiles/models.py
@@ -9,7 +9,7 @@ class Profile(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
-    image = models.ImageField(upload_to='images/', default='../default_user_t2amq9.png')  # noqa
+    image = models.ImageField(upload_to='images/', default='../default_user_dpyhbv')  # noqa
 
     class Meta:
         ordering = ['-created_at']
