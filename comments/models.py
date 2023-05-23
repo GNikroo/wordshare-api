3d5a468866d9fd1c720598f217ca282f56100b4e
GNikroo <gina.nikroo@gmail.com>
2023-05-23 11:42:16 +0000

    Add comment filter to post filterset

--- comments/models.py
+++ comments/models.py
@@ -5,7 +5,7 @@ from posts.models import Post
 
 class Comment(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='comment', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     content = models.TextField()
--- posts/views.py
+++ posts/views.py
@@ -21,7 +21,7 @@ class PostList(generics.ListCreateAPIView):
     filterset_fields = [
         'owner__followed__owner__profile',
         'likes__owner__profile',
-        'owner__profile'
+        'comment__owner__profile'
     ]
     search_fields = [
         'owner__username',
