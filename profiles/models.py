90421adf44c044f87a9c1527946fb10b457b2aff
GNikroo <gina.nikroo@gmail.com>
2023-05-25 09:47:46 +0000

    Update database

--- profiles/models.py
+++ profiles/models.py
@@ -9,7 +9,7 @@ class Profile(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
-    image = models.ImageField(upload_to='images/', default='../default_user_qjdswn')  # noqa
+    image = models.ImageField(upload_to='images/', default='../default_user_npahk9')  # noqa
 
     class Meta:
         ordering = ['-created_at']
